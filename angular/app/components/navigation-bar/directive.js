c5929f10aef066ae6974ddcff6575e504a1f2430
jacoblee <jacoblee@jacoblee-macbookpro.roam.corp.google.com>
2016-02-28 14:19:31 -0800

    Removes logout button from login page

--- angular/app/components/navigation-bar/directive.js
+++ angular/app/components/navigation-bar/directive.js
@@ -1,7 +1,12 @@
 app.directive('navigationBar', function() {
 
   return {
-    templateUrl: 'components/navigation-bar/template.html'
+    templateUrl: 'components/navigation-bar/template.html',
+    controller: ['$scope', '$location', function($scope, $location) {
+      $scope.hideOn = function(viewLocation) {
+        return viewLocation === $location.path();
+      };
+    }]
   };
 
 });
